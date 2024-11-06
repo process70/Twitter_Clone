bffe526ff05a4dff5d3b61a26b0350b68fb1b6d7
process70 <processhandler70@gmail.com>
2024-11-06 19:17:32 +0000

    Initial commit

---
+++ frontend/vite.config.js
@@ -0,0 +1,27 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    proxy: {
+			"/auth": {
+				target: "http://localhost:5000",
+				changeOrigin: true,
+			},
+			"/users": {
+				target: "http://localhost:5000",
+				changeOrigin: true,
+			},
+			"/posts": {
+				target: "http://localhost:5000",
+				changeOrigin: true,
+			},
+			"/notifications": {
+				target: "http://localhost:5000",
+				changeOrigin: true,
+			},
+	},
+  }
+})
